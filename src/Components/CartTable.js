6d5129ff7e65f5943f7497f12b2567a7fd468983
Ilya Prusakou <beyourself@tut.by>
2020-04-25 18:17:53 +0400

    Table Good

--- src/Components/CartTable.js
+++ src/Components/CartTable.js
@@ -1,109 +1,250 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const TableDiv = styled.div`
+	margin: 10px 70px 70px;
+	box-shadow: 0px 35px 50px rgba( 0, 0, 0, 0.2 );
+	@media (max-width: 767px) {
+		&:before{
+			content: "Scroll horizontally >";
+			display: block;
+			text-align: right;
+			font-size: 11px;
+			color: black;
+			padding: 0 0 10px;
+		}
+	}
+
+
+
+`;
 const Table = styled.table`
-    width: 800px;
-    border-collapse: collapse;
-    overflow: hidden;
-    box-shadow: 0 0 20px rgba(0,0,0,0.1);
+	border-radius: 5px;
+	font-size: 12px;
+	font-weight: normal;
+	border: none;
+	border-collapse: collapse;
+	width: 100%;
+	max-width: 100%;
+	white-space: nowrap;
+	background-color: white;
+
+	@media (max-width: 767px) {
+			display: block;
+			width: 100%;
+		}
+
 `;
 const Th = styled.th`
-    padding: 15px;
-    background-color: rgba(255,255,255,0.2);
-    color: #fff;
-    text-align: left;
+	font-family: 'Roboto Thin';
+	text-align: center;
+	padding: 8px;
+	color: black;
+	background: #dfdfdf;
+	&:nth-child(odd) {
+		color: #ffffff;
+    	background: #4a4a4a;
+	}
+	@media (max-width: 767px) {
+		display: block;
+
+		padding: 20px .625em .625em .625em;
+        height: 60px;
+        vertical-align: middle;
+        box-sizing: border-box;
+        overflow-x: hidden;
+        overflow-y: auto;
+        width: 120px;
+        font-size: 13px;
+		text-overflow: ellipsis;
+
+		text-align: left;
+        border-bottom: 1px solid #f7f7f9;
+
+		&:last-child{
+			border-bottom: none;
+		}
+	}
 `;
 const Td = styled.td`
-    padding: 15px;
-    background-color: rgba(255,255,255,0.2);
-    color: #fff;
+	text-align: center;
+	padding: 8px;
+	border-bottom: 0.8px solid black;
+	font-size: 12px;
+	@media (max-width: 767px) {
+		padding: 20px .625em .625em .625em;
+        height: 60px;
+        vertical-align: middle;
+        box-sizing: border-box;
+        overflow-x: hidden;
+        overflow-y: auto;
+        width: 120px;
+        font-size: 13px;
+		text-overflow: ellipsis;
+
+		tr &:nth-child(odd) {
+			background: #F8F8F8;
+			border-right: 1px solid #E6E4E4;
+		}
+
+		tr &:nth-child(even) {
+			border-right: 1px solid #E6E4E4;
+		}
+
+		tbody & {
+			display: block;
+        	text-align: center;
+		}
+
+	}
+`;
+const Tr = styled.tr`
+	&:nth-child(even) {
+		background: #F8F8F8;
+	}
+	&:hover {
+		background-color: #dfdfdf;
+	}
+	@media (max-width: 767px) {
+		tbody & {
+			display: table-cell;
+		}
+		tbody &:nth-child(odd) {
+			background: none;
+		}
+		&:nth-child(even){
+			background: transparent;
+		}
+	}
+
+
 `;
 
 const Thead = styled.thead`
- & {
-	    th {
-		    background-color: #55608f;
-	        }
-    }
+	@media (max-width: 767px) {
+		display: block;
+		float: left;
+	}
+`;
+const Tbody = styled.tbody`
+	@media (max-width: 767px) {
+		display: block;
+		width: auto;
+        position: relative;
+        overflow-x: auto;
+	}
 `;
 
-const TBody = styled.tbody`
-& {
-	tr {
-		&:hover {
-			background-color: rgba(255,255,255,0.3);
-		}
-	}
-	td {
-		position: relative;
-		&:hover {
-			&:before {
-				content: "";
-				position: absolute;
-				left: 0;
-				right: 0;
-				top: -9999px;
-				bottom: -9999px;
-				background-color: rgba(255,255,255,0.2);
-				z-index: -1;
-			}
-		}
-	}
-}
-`;
 
 function CartTable(props){
     return (
-        <div>
-	<Table>
-		<Thead>
-			<tr>
-				<Th>Column 1</Th>
-				<Th>Column 2</Th>
-				<Th>Column 3</Th>
-				<Th>Column 4</Th>
-				<Th>Column 5</Th>
-			</tr>
-		</Thead>
-		<TBody>
-			<tr>
-				<Td>Cell 1</Td>
-				<Td>Cell 2</Td>
-				<Td>Cell 3</Td>
-				<Td>Cell 4</Td>
-				<Td>Cell 5</Td>
-			</tr>
-			<tr>
-				<Td>Cell 1</Td>
-				<Td>Cell 2</Td>
-				<Td>Cell 3</Td>
-				<Td>Cell 4</Td>
-				<Td>Cell 5</Td>
-			</tr>
-			<tr>
-				<Td>Cell 1</Td>
-				<Td>Cell 2</Td>
-				<Td>Cell 3</Td>
-				<Td>Cell 4</Td>
-				<Td>Cell 5</Td>
-			</tr>
-			<tr>
-				<Td>Cell 1</Td>
-				<Td>Cell 2</Td>
-				<Td>Cell 3</Td>
-				<Td>Cell 4</Td>
-				<Td>Cell 5</Td>
-			</tr>
-			<tr>
-				<Td>Cell 1</Td>
-				<Td>Cell 2</Td>
-				<Td>Cell 3</Td>
-				<Td>Cell 4</Td>
-				<Td>Cell 5</Td>
-			</tr>
-		</TBody>
-	</Table>
-</div>
+        <TableDiv>
+    		<Table>
+        	<Thead>
+        		<Tr>
+            		<Th>Артикул</Th>
+            		<Th>Цвет</Th>
+            		<Th>Размер</Th>
+            		<Th>Количество</Th>
+            		<Th>Цена</Th>
+					<Th>Отложить</Th>
+					<Th>Удалить</Th>
+        		</Tr>
+        	</Thead>
+        	<Tbody>
+        		<Tr>
+            		<Td>Content 1</Td>
+            		<Td>Content 1</Td>
+            		<Td>Content 1</Td>
+            		<Td>Content 1</Td>
+            		<Td>Content 1</Td>
+					<Td>Content 1</Td>
+            		<Td>Content 1</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 2</Td>
+            		<Td>Content 2</Td>
+            		<Td>Content 2</Td>
+            		<Td>Content 2</Td>
+            		<Td>Content 2</Td>
+					<Td>Content 2</Td>
+            		<Td>Content 2</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 3</Td>
+            		<Td>Content 3</Td>
+            		<Td>Content 3</Td>
+            		<Td>Content 3</Td>
+            		<Td>Content 3</Td>
+					<Td>Content 3</Td>
+            		<Td>Content 3</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 4</Td>
+            		<Td>Content 4</Td>
+            		<Td>Content 4</Td>
+            		<Td>Content 4</Td>
+            		<Td>Content 4</Td>
+					<Td>Content 4</Td>
+            		<Td>Content 4</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 5</Td>
+            		<Td>Content 5</Td>
+            		<Td>Content 5</Td>
+            		<Td>Content 5</Td>
+            		<Td>Content 5</Td>
+					<Td>Content 5</Td>
+            		<Td>Content 5</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 6</Td>
+            		<Td>Content 6</Td>
+            		<Td>Content 6</Td>
+           	 		<Td>Content 6</Td>
+            		<Td>Content 6</Td>
+					<Td>Content 6</Td>
+            		<Td>Content 6</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 7</Td>
+            		<Td>Content 7</Td>
+            		<Td>Content 7</Td>
+            		<Td>Content 7</Td>
+            		<Td>Content 7</Td>
+					<Td>Content 7</Td>
+            		<Td>Content 7</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 8</Td>
+            		<Td>Content 8</Td>
+            		<Td>Content 8</Td>
+            		<Td>Content 8</Td>
+           			<Td>Content 8</Td>
+					<Td>Content 8</Td>
+            		<Td>Content 8</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 9</Td>
+            		<Td>Content 9</Td>
+            		<Td>Content 9</Td>
+            		<Td>Content 9</Td>
+            		<Td>Content 9</Td>
+					<Td>Content 9</Td>
+            		<Td>Content 9</Td>
+        		</Tr>
+        		<Tr>
+            		<Td>Content 10</Td>
+            		<Td>Content 10</Td>
+            		<Td>Content 10</Td>
+            		<Td>Content 10</Td>
+            		<Td>Content 10</Td>
+					<Td>Content 10</Td>
+            		<Td>Content 10</Td>
+        		</Tr>
+        	</Tbody>
+    	</Table>
+	</TableDiv>
     );
 }
 
